616b86261fd1c89c56d81816ec831b15b97b3b66
Daniel Hammond <daniel@twotoasters.com>
2011-06-12 22:48:48 -0400

    init

---
+++ modbranchmerge.rb
@@ -0,0 +1,54 @@
+require 'digest/md5'
+
+def branch(name)
+  system("git branch #{name}")
+end
+
+def checkout(name)
+  system("git checkout #{name}")
+end
+
+def delete(name)
+  system("git branch -d #{name}")
+end
+
+def mod
+  File.open("data",'a+') {|f| f.write(Time.now)}
+end
+
+def commit
+  system("git commit -am '.'")
+end
+
+def merge(name)
+  system("git merge #{name}")
+end
+
+def modbranchmerge(name,many)
+  branches = []
+  many.times do
+    newbranch = Digest::MD5.hexdigest(Time.now.to_s)
+    branches << newbranch
+    sleep(1)
+  end
+  branches.each do |b|
+    branch(b)
+  end
+  branches.each do |b|
+    checkout(b)
+    File.open(b,'a+') {|f| f.write(Time.now)}
+    system("git add .")
+    commit
+    modbranchmerge(b,rand(5)) if rand < 0.25
+    checkout(name)
+  end
+  checkout(name)
+  File.open("data2",'a+') {|f| f.write(Time.now)}
+  commit
+  branches.each do |b| 
+    merge(b)
+    commit
+    delete(b)
+  end
+end
+modbranchmerge("master",5)
